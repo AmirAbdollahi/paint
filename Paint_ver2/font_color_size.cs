090c9452cd4daf470eac8f65a8a74d0e3f49eaf6
Amir Abdollahi <amir.abdollahi69@gmail.com>
2018-02-08 11:56:48 +0330

    Add project files.

---
+++ Paint_ver2/font_color_size.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Paint_ver2
+{
+    class font_color_size
+    {
+        public int fontsize = 4;
+        public Color p = Color.Black;
+        public Brush pp = Brushes.Black;
+        public bool resultline = false;
+        public bool resultpencil = false;
+
+
+
+
+        public int font_size(int x)
+        {
+            fontsize = x;
+            return fontsize;
+        }
+
+       
+      
+
+        
+    }
+}
